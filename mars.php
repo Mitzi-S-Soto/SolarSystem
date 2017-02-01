b48aa53d48594d6bcb48358a7cefc23b2787ecd6
Mitzi S <contact@mitzisunshine.com>
2017-02-01 13:46:29 -0500

    Add files via upload

---
+++ mars.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mars : Solar System Model</title>
+        <!--Normalize CSS-->
+        <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+        <!--Style.css-->
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <!--Google Fonts-->
+        <?php include 'includes/google-fonts.html';?>
+    </head>
+    
+    <body>
+        <!--Include Header-->
+        <?php include 'includes/header.html';?>
+    
+        <div id="container">
+        
+            
+        </div><!--End Container-->
+        
+        <!--Include Footer-->
+        <?php include 'includes/footer.html';?>
+    </body>
+</html>
